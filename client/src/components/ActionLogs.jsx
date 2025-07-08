54830737e44cb474d8624a198843969b03f8649f
Mamta Kumari Vyas <mamtavyas1990@gmail.com>
2025-07-08 18:38:31 +0530

    Fixed useEffect dependency warning in ActionLogs.jsx

--- client/src/components/ActionLogs.jsx
+++ client/src/components/ActionLogs.jsx
@@ -19,7 +19,7 @@ function ActionLogs({ onClose }) {
     };
 
     fetchLogs();
-  }, []);
+  }, [API_BASE]);
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50 overflow-auto">
